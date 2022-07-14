eacc9c56343752ec0a9d700f19b69d646682f4a8
Andrew Mayorov <encube.ul@gmail.com>
2022-07-14 20:05:03 +0300

    Move unused msgpack protocol out to thrift runtime library

--- src/dmt_api_thrift_utils.erl
+++ src/dmt_api_thrift_utils.erl
@@ -6,37 +6,15 @@
 -type thrift_type() :: term().
 -type thrift_value() :: term().
 
--spec encode
-    (binary, thrift_type(), thrift_value()) -> binary();
-    (msgpack, thrift_type(), thrift_value()) -> dmsl_msgpack_thrift:'Value'().
+-spec encode(binary, thrift_type(), thrift_value()) -> binary().
 encode(binary, Type, Value) ->
     Codec0 = thrift_strict_binary_codec:new(),
     {ok, Codec} = thrift_strict_binary_codec:write(Codec0, Type, Value),
-    Data = thrift_strict_binary_codec:close(Codec),
-    Data;
-encode(Proto, Type, Value) ->
-    {ok, Proto0} = new_protocol(Proto),
-    {Proto1, ok} = thrift_protocol:write(Proto0, {Type, Value}),
-    {_Proto, {ok, Data}} = thrift_protocol:close_transport(Proto1),
-    Data.
+    thrift_strict_binary_codec:close(Codec).
 
--spec decode
-    (binary, thrift_type(), binary()) -> thrift_value();
-    (msgpack, thrift_type(), dmsl_msgpack_thrift:'Value'()) -> thrift_value().
+-spec decode(binary, thrift_type(), binary()) -> thrift_value().
 decode(binary, Type, Data) ->
     Codec = thrift_strict_binary_codec:new(Data),
     {ok, Value, Leftovers} = thrift_strict_binary_codec:read(Codec, Type),
     <<>> = thrift_strict_binary_codec:close(Leftovers),
-    Value;
-decode(Proto, Type, Data) ->
-    {ok, Proto0} = new_protocol(Proto, Data),
-    {_Proto, {ok, Value}} = thrift_protocol:read(Proto0, Type),
     Value.
-
-%%
-
-new_protocol(msgpack) ->
-    dmt_api_thrift_msgpack_protocol:new().
-
-new_protocol(msgpack, Data) ->
-    dmt_api_thrift_msgpack_protocol:new(Data).
